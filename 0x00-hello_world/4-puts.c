37908a22f6c85e95ba19557db41f436f490ffcfa
youssef lafkih <lafkih581@gmail.com>
2023-03-16 18:37:29 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *  main -Entry point
+ *  main -printing a word using puts
  *
  *  Return: Always 0 (Success)
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle);
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
