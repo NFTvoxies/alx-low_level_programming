a8af10d6852c6874afb04de6c5504e3e8488bdfc
youssef lafkih <lafkih581@gmail.com>
2023-03-23 18:47:06 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    long int n = 612852475143;
+    long int i;
+
+    while (n % 2 == 0)
+    {
+        n /= 2;
+    }
+
+    for (i = 3; i <= sqrt(n); i += 2)
+    {
+        while (n % i == 0)
+        {
+            n /= i;
+        }
+    }
+
+    if (n > 2)
+    {
+        printf("%ld\n", n);
+    }
+
+    return 0;
+}
+
