2ee8bd9a5b8b79dfe9860de55d975ecd23af8712
youssef lafkih <lafkih581@gmail.com>
2023-03-23 18:49:09 -0700

    nested loops

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
-#include <math.h>
 
 int main(void)
 {
     long int n = 612852475143;
-    long int i;
+    long int i, largest_prime_factor;
 
     while (n % 2 == 0)
     {
         n /= 2;
+        largest_prime_factor = 2;
     }
 
-    for (i = 3; i <= sqrt(n); i += 2)
+    for (i = 3; i * i <= n; i += 2)
     {
         while (n % i == 0)
         {
             n /= i;
+            largest_prime_factor = i;
         }
     }
 
     if (n > 2)
     {
-        printf("%ld\n", n);
+        largest_prime_factor = n;
     }
+
+    printf("%ld\n", largest_prime_factor);
 
     return 0;
 }
